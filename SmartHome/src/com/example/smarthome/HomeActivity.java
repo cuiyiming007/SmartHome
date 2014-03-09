91fed6ffd32cdc408d4540949eff7bf696f6edad
huangyulu <408975300@qq.com>
2014-03-09 13:31:01 +0800

    add UI architecture and the homeactivity image

--- SmartHome/src/com/example/smarthome/HomeActivity.java
+++ SmartHome/src/com/example/smarthome/HomeActivity.java
@@ -1,8 +1,18 @@
 package com.example.smarthome;
 
+//import com.gdgl.adapter.ImageForGridViewAdapter;
+
+import com.gdgl.adapter.ImageForGridViewAdapter;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
+import android.widget.GridView;
 
 public class HomeActivity extends Activity {
 
@@ -10,6 +20,17 @@ public class HomeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		GridView gridview = (GridView) findViewById(R.id.homegridview);
+		gridview.setAdapter(new ImageForGridViewAdapter(this));
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				Toast.makeText(HomeActivity.this, "" + arg2, Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	@Override
