6d56aced225d25c15203366576c699d89a6d8061
joyzhu <joyzhu666@gmail.com>
2014-03-18 22:05:48 +0800

    layout

--- SmartHome/src/com/gdgl/smarthome/SelectPicPopupWindow.java
+++ SmartHome/src/com/gdgl/smarthome/SelectPicPopupWindow.java
@@ -30,31 +30,20 @@ public class SelectPicPopupWindow extends PopupWindow {
 		int h = context.getWindowManager().getDefaultDisplay().getHeight();
 		int w = context.getWindowManager().getDefaultDisplay().getWidth();
 		btn_cancel = (Button) mMenuView.findViewById(R.id.btn_cancel);
-		// ȡ��ť
 		btn_cancel.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				// ��ٵ�����
 				SaveDate.saveDate(context, new OAuthV2());
 				context.finish();
 			}
 		});
-		// ���ð�ť����
-		// ����SelectPicPopupWindow��View
 		this.setContentView(mMenuView);
-		// ����SelectPicPopupWindow��������Ŀ�
 		this.setWidth(w / 2 + 50);
-		// ����SelectPicPopupWindow��������ĸ�
 		this.setHeight(LayoutParams.WRAP_CONTENT);
-		// ����SelectPicPopupWindow��������ɵ��
 		this.setFocusable(true);
-		// ����SelectPicPopupWindow�������嶯��Ч��
 		this.setAnimationStyle(R.style.mystyle);
-		// ʵ��һ��ColorDrawable��ɫΪ��͸��
 		ColorDrawable dw = new ColorDrawable(0000000000);
-		// ����SelectPicPopupWindow��������ı���
 		this.setBackgroundDrawable(dw);
-		// mMenuView���OnTouchListener�����жϻ�ȡ����λ�������ѡ�����������ٵ�����
 		mMenuView.setOnTouchListener(new OnTouchListener() {
 
 			public boolean onTouch(View v, MotionEvent event) {
