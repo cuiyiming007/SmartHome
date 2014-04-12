6de44f95c0d2f2cb24667c3bbcb3993170392498
joyzhu <joyzhu666@gmail.com>
2014-04-12 17:35:26 +0800

    update SwitchControlFragment
    update SwitchControlFragment

--- SmartHome/src/com/gdgl/activity/SwitchControlFragment.java
+++ SmartHome/src/com/gdgl/activity/SwitchControlFragment.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.gdgl.adapter.DevicesBaseAdapter;
+import com.gdgl.manager.LightManager;
 import com.gdgl.manager.Manger;
+import com.gdgl.manager.UIListener;
 import com.gdgl.model.DevicesModel;
 import com.gdgl.model.SimpleDevicesModel;
 import com.gdgl.smarthome.R;
@@ -13,147 +15,186 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.database.CursorJoiner.Result;
 import android.os.Bundle;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 @SuppressLint("NewApi")
-public class SwitchControlFragment extends BaseControlFragment {
-
+public class SwitchControlFragment extends BaseControlFragment implements 
+		UIListener {
+	boolean[] mBoolean = { false, false, false };
+	List<String> mName = new ArrayList<String>();
+	List<String> mIeee = new ArrayList<String>();
+	View mView;
+	int mCount;
+	SimpleDevicesModel mDevices;
+    
+	LightManager mLightManager;
 	
+	TextView txt_devices_name, txt_devices_region;
 
-    boolean[] mBoolean = { false, false, false };
-    List<String> mName=new ArrayList<String>();
-    View mView;
-    int mCount;
-    SimpleDevicesModel  mDevices;
+	CheckBox mSwitch1, mSwitch2, mSwitch3;
+	TextView mSwichName1, mSwichName2, mSwichName3;
+	RelativeLayout viewGroup1, viewGroup2;
 
-    TextView txt_devices_name, txt_devices_region;
+	@SuppressLint("NewApi")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
 
-    CheckBox mSwitch1, mSwitch2, mSwitch3;
-    TextView mSwichName1, mSwichName2, mSwichName3;
-    RelativeLayout viewGroup1, viewGroup2;
+		Bundle extras = getArguments();
+		if (null != extras) {
+			mDevices = (SimpleDevicesModel) extras
+					.getParcelable(DevicesBaseAdapter.PASS_OBJECT);
+		}
+		initstate();
+	}
 
-    @SuppressLint("NewApi")
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        super.onCreate(savedInstanceState);
-
-        Bundle extras = getArguments();
-        if (null != extras) {
-            mDevices = (SimpleDevicesModel) extras
-                    .getParcelable(DevicesBaseAdapter.PASS_OBJECT);
-        }
-        initstate();
-    }
-
-    private void initstate() {
-        // TODO Auto-generated method stub
-        if (null != mDevices) {
-        	String s=mDevices.getmOnOffStatus();
-        	String name=mDevices.getmNodeENNAme();
-        	String[] result = s.split(",");
-        	String[] nameR=name.split(",");
-        	for(int n=0;n<nameR.length;n++){
-        		if(nameR[n].trim().equals("")){
-        			mName.add("Switch_"+n);
-        		}else{
-        			mName.add(nameR[n]);
-        		}
-        	}
-        	for(int m=0;m<result.length;m++){
-        		if(result[m].trim().equals("1")){
-					mBoolean[m]=true;
+	private void initstate() {
+		// TODO Auto-generated method stub
+		mLightManager=LightManager.getInstance();
+		mLightManager.addObserver(SwitchControlFragment.this);
+		if (null != mDevices) {
+			String s = mDevices.getmOnOffStatus();
+			String name = mDevices.getmNodeENNAme();
+			String ieee = mDevices.getmNodeENNAme();
+			String[] result = s.split(",");
+			String[] nameR = name.split(",");
+			String[] Ieee = ieee.split(",");
+			for (int n = 0; n < nameR.length; n++) {
+				if (nameR[n].trim().equals("")) {
+					mName.add("Switch_" + n);
+				} else {
+					mName.add(nameR[n]);
 				}
-        	}
-        	 mCount = result.length;
-        }
-    }
+			}
+			for (int m = 0; m < result.length; m++) {
+				if (result[m].trim().equals("1")) {
+					mBoolean[m] = true;
+				}
+			}
+			for (int i = 0; i < Ieee.length; i++) {
+				if (!Ieee[i].trim().equals("")) {
+					mIeee.add(Ieee[i]);
+				}
+			}
+			mCount = result.length;
+		}
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        mView = inflater.inflate(R.layout.switch_control, null);
-        initView();
-        return mView;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		mView = inflater.inflate(R.layout.switch_control, null);
+		initView();
+		return mView;
+	}
 
-    private void initView() {
-        // TODO Auto-generated method stub
-        mSwitch1 = (CheckBox) mView.findViewById(R.id.switch_state1);
-        mSwitch2 = (CheckBox) mView.findViewById(R.id.switch_state2);
-        mSwitch3 = (CheckBox) mView.findViewById(R.id.switch_state3);
+	private void initView() {
+		// TODO Auto-generated method stub
+		mSwitch1 = (CheckBox) mView.findViewById(R.id.switch_state1);
+		mSwitch2 = (CheckBox) mView.findViewById(R.id.switch_state2);
+		mSwitch3 = (CheckBox) mView.findViewById(R.id.switch_state3);
+		
+		
 
-        mSwichName1 = (TextView) mView.findViewById(R.id.switch_name1);
-        mSwichName2 = (TextView) mView.findViewById(R.id.switch_name2);
-        mSwichName3 = (TextView) mView.findViewById(R.id.switch_name3);
+		mSwichName1 = (TextView) mView.findViewById(R.id.switch_name1);
+		mSwichName2 = (TextView) mView.findViewById(R.id.switch_name2);
+		mSwichName3 = (TextView) mView.findViewById(R.id.switch_name3);
 
-        txt_devices_name = (TextView) mView.findViewById(R.id.txt_devices_name);
-        txt_devices_region = (TextView) mView
-                .findViewById(R.id.txt_devices_region);
+		txt_devices_name = (TextView) mView.findViewById(R.id.txt_devices_name);
+		txt_devices_region = (TextView) mView
+				.findViewById(R.id.txt_devices_region);
 
-        txt_devices_name.setText(mDevices.getmName());
-        txt_devices_region.setText(mDevices.getmDeviceRegion());
+		txt_devices_name.setText(mDevices.getmName());
+		txt_devices_region.setText(mDevices.getmDeviceRegion());
 
-        viewGroup1 = (RelativeLayout) mView.findViewById(R.id.switch_group1);
-        viewGroup2 = (RelativeLayout) mView.findViewById(R.id.switch_group2);
+		viewGroup1 = (RelativeLayout) mView.findViewById(R.id.switch_group1);
+		viewGroup2 = (RelativeLayout) mView.findViewById(R.id.switch_group2);
 
-        switch (mCount) {
-        case 1:
-            viewGroup1.setVisibility(View.VISIBLE);
-            viewGroup2.setVisibility(View.GONE);
-            mSwitch1.setChecked(mBoolean[0]);
-            mSwichName1.setText(mName.get(0));
-            break;
-        case 2:
-            viewGroup1.setVisibility(View.GONE);
-            viewGroup2.setVisibility(View.VISIBLE);
-            mSwitch2.setChecked(mBoolean[0]);
-            mSwitch3.setChecked(mBoolean[1]);
+		switch (mCount) {
+		case 1:
+			viewGroup1.setVisibility(View.VISIBLE);
+			viewGroup2.setVisibility(View.GONE);
+			mSwitch1.setChecked(mBoolean[0]);
+			mSwichName1.setText(mName.get(0));
+			mSwitch1.setOnClickListener(new SwitchClickListener( mIeee.get(0), mBoolean[0]));
+			break;
+		case 2:
+			viewGroup1.setVisibility(View.GONE);
+			viewGroup2.setVisibility(View.VISIBLE);
+			mSwitch2.setChecked(mBoolean[0]);
+			mSwitch3.setChecked(mBoolean[1]);
+			
+			mSwitch2.setOnClickListener(new SwitchClickListener( mIeee.get(0), mBoolean[0]));
+			mSwitch3.setOnClickListener(new SwitchClickListener( mIeee.get(1), mBoolean[1]));
+			
+			mSwichName2.setText(mName.get(0));
+			mSwichName3.setText(mName.get(1));
+			break;
+		case 3:
+			mSwitch1.setChecked(mBoolean[0]);
+			mSwitch2.setChecked(mBoolean[1]);
+			mSwitch3.setChecked(mBoolean[2]);
+			
+			mSwitch1.setOnClickListener(new SwitchClickListener( mIeee.get(0), mBoolean[0]));
+			mSwitch2.setOnClickListener(new SwitchClickListener( mIeee.get(1), mBoolean[1]));
+			mSwitch3.setOnClickListener(new SwitchClickListener( mIeee.get(2), mBoolean[2]));
+			
+			mSwichName1.setText(mName.get(0));
+			mSwichName2.setText(mName.get(1));
+			mSwichName3.setText(mName.get(2));
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public class SwitchClickListener implements OnClickListener{
+		String CIeee;
+		boolean CB;
+		public SwitchClickListener(String ieee,boolean b){
+			CIeee=ieee;
+			CB=b;
+		}
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			//mLightManager.OnOffLightSwitchOperation();
+		}
+		
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		if (!(activity instanceof UpdateDevice)) {
+			throw new IllegalStateException("Activity必须实现SaveDevicesName接口");
+		}
+		mUpdateDevice = (UpdateDevice) activity;
+		super.onAttach(activity);
+	}
 
-            mSwichName2.setText(mName.get(0));
-            mSwichName3.setText(mName.get(1));
-            break;
-        case 3:
-            mSwitch1.setChecked(mBoolean[0]);
-            mSwitch2.setChecked(mBoolean[1]);
-            mSwitch3.setChecked(mBoolean[2]);
-            
-            mSwichName1.setText(mName.get(0));
-            mSwichName2.setText(mName.get(1));
-            mSwichName3.setText(mName.get(2));
-            break;
-        default:
-            break;
-        }
-    }
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		mLightManager.deleteObserver(SwitchControlFragment.this);
+		super.onDestroy();
+	}
 
-    @Override
-    public void onAttach(Activity activity) {
-        // TODO Auto-generated method stub
-        if (!(activity instanceof UpdateDevice)) {
-            throw new IllegalStateException("Activity必须实现SaveDevicesName接口");
-        }
-        mUpdateDevice = (UpdateDevice) activity;
-        super.onAttach(activity);
-    }
+	@Override
+	public void editDevicesName() {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void onDestroy() {
-        // TODO Auto-generated method stub
-        super.onDestroy();
-    }
-
-    @Override
-    public void editDevicesName() {
-        // TODO Auto-generated method stub
-
-    }
+	}
 
 	@Override
 	public void update(Manger observer, Object object) {
