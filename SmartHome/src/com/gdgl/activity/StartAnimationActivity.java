e304623cdaa539e1e879ae3ffcfea30f11bd56ee
justek-Wade <huangyulu81@gmail.com>
2014-04-14 23:47:01 +0800

    release the Animation memory again

--- SmartHome/src/com/gdgl/activity/StartAnimationActivity.java
+++ SmartHome/src/com/gdgl/activity/StartAnimationActivity.java
@@ -10,13 +10,14 @@ import com.gdgl.smarthome.R;
 
 public class StartAnimationActivity extends Activity {
 	private AnimationDrawable animationDrawable;
+	ImageView image;
     private final static long SPLASH_DELAY_MILLIS=6000;
 	protected void onCreate(android.os.Bundle savedInstanceState) 
 	{
 		 super.onCreate(savedInstanceState); 
 		  setContentView(R.layout.startanimation); 
 		 
-		  ImageView image = (ImageView) findViewById(R.id.animationimage); 
+		   image = (ImageView) findViewById(R.id.animationimage); 
 		  image.setBackgroundResource(R.drawable.startanimation);  
 		  animationDrawable = (AnimationDrawable) image.getBackground(); 
 		  new Handler().postDelayed(new Runnable() {
@@ -28,12 +29,24 @@ public class StartAnimationActivity extends Activity {
 	private void goHome() {
 		Intent intent = new Intent(StartAnimationActivity.this, SmartHome.class);
 		StartAnimationActivity.this.startActivity(intent);
+		
 		StartAnimationActivity.this.finish();
 	}
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
-		animationDrawable.start();
+		if (animationDrawable!=null) {
+			animationDrawable.start();
+		}
+//		animationDrawable=null;
+//		System.gc();
 		super.onWindowFocusChanged(hasFocus);
+	}
+	@Override
+	public void finish() {
+		((AnimationDrawable)(image.getBackground())).stop();
+		image.setBackgroundDrawable(null);
+		animationDrawable=null;
+		super.finish();
 	}
 
 }
