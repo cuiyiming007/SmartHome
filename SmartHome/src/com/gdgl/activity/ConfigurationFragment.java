3654bbf004f865b7dc7c9967db01a7054d1270bd
Trice <Trice@Trice-GuangLian>
2015-06-16 10:39:26 +0800

    add configuration page

---
+++ SmartHome/src/com/gdgl/activity/ConfigurationFragment.java
@@ -0,0 +1,27 @@
+package com.gdgl.activity;
+
+import com.gdgl.smarthome.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ConfigurationFragment extends Fragment {
+	View mView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		mView = inflater.inflate(R.layout.configurationfragment, null);
+		return mView;
+	}
+}
